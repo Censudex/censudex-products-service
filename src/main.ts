ffd2853092b4ab0c1d981267e951792ef660ea36
grenzya <ignacio.bugueno02@alumnos.ucn.cl>
2025-10-21 00:55:56 -0300

    feat: implement GRPC microservice for products with appropriate configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
+/**
+ * Función principal para arrancar el servicio (microservicio) de productos.
+ */
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'products',
+        protoPath: join(__dirname, 'proto/products.proto'),
+        url: '0.0.0.0:50051',
+      },
+    },
+  );
+
+  await app.listen();
+  console.log('Products Service se está ejecutando en el puerto 50051');
 }
 bootstrap();
